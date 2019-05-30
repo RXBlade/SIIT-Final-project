e568630970c5d6a359efab8fe4631cfd89e8a693
RedXenonBlade <redxenonblade@RXBlade.RXBlade.com>
2019-05-30 15:32:35 -0400

    Added Date to invoices

--- pages/invoice/invoice.js
+++ pages/invoice/invoice.js
@@ -2,6 +2,7 @@ var urlParams = new URLSearchParams(location.search);
 const urlId = urlParams.get('id');
 const urlSeries = urlParams.get('series');
 const urlNumber = urlParams.get('number');
+const urlDate = urlParams.get('date');
 var id = 1;
 
 fetch(`http://alpha.apexcode.ro/api/invoices/1/items`)
@@ -10,6 +11,7 @@ fetch(`http://alpha.apexcode.ro/api/invoices/1/items`)
     
     document.getElementById('series').innerHTML = urlSeries;
     document.getElementById('number').innerHTML = urlNumber;
+    document.getElementById('date').innerHTML = urlDate;
     resp.forEach(item => {
         let value = item.Quantity * item.Price;
         document.getElementById('main').innerHTML += 
@@ -35,7 +37,6 @@ fetch(`http://alpha.apexcode.ro/api/invoices/1/items`)
         <td><i class="material-icons">edit</i><i class="material-icons">close</i></td>
 
     </tr>`
-    id++;    
+    id++;
     });
-    
 })
